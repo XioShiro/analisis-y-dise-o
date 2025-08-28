0e86a91e4284384041d000517388441d52a04288
Xiomara <xioshiro707@gmail.com>
2025-08-28 12:52:33 -0500

    mejorando el ordenamiento

--- Pruebas.java
+++ Pruebas.java
@@ -6,31 +6,11 @@ package com.mycompany.pruebas;
 
 /**
  *
- * @author Usuario
+ * @author LAB-USR-AREQUIPA
  */
-
 public class Pruebas {
+
     public static void main(String[] args) {
-        int[] a = {10, 2, 6, 9, 4};
-
-        System.out.print("Antes:  ");
-        for (int x : a) System.out.print(x + " ");
-        System.out.println();
-
-        int n = a.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1 - i; j++) {
-                if (a[j] > a[j + 1]) {
-                    int t = a[j];
-                    a[j] = a[j + 1];
-                    a[j + 1] = t;
-                }
-            }
-        } 
-
-        System.out.println("Despues: ");
-        for (int x : a) System.out.print(x + " ");
-        System.out.println();
+        System.out.println("Hello World!");
     }
 }
-
